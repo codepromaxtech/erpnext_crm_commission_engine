ebb1e7b5b9cf53138e33fb85f39ffa5aceeefb78
CodeProMax Tech <codepromaxtech@gmail.com>
2026-04-01 15:46:40 +0000

    feat: level title for commission defaults
    - Added level_title to Commission Level Default child table
    - Allows managers to visually name Hierarchy Levels (e.g., Area Manager, District Manager) in Commission Settings

--- commission_engine/commission_engine/doctype/commission_level_default/commission_level_default.py
+++ commission_engine/commission_engine/doctype/commission_level_default/commission_level_default.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2026, CodeProMax Tech and contributors
 # For license information, please see license.txt
 
+from typing import TYPE_CHECKING
 from frappe.model.document import Document
 
+if TYPE_CHECKING:
+	from frappe.types import DF
+
+	hierarchy_level: DF.Int
+	level_title: DF.Data | None
+	onetime_pct: DF.Percent
+	parent: DF.Data
+	parentfield: DF.Data
+	parenttype: DF.Data
 
 class CommissionLevelDefault(Document):
 	pass
